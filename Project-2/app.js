1292fe5c6ef460b6af46ae157b344e6975108e97
mertatc <mertatc1903@hotmail.com>
2023-04-30 11:58:23 +0300

    a

--- Project-2/app.js
+++ Project-2/app.js
@@ -35,6 +35,23 @@ function update(){
     })
 
     const actives = document.querySelectorAll(".active");
+
+    //console.log(actives.length, circles.length);
+    //console.log((actives.length / circles.length)*100);
+    //console.log((actives.length-1)/ (circles.length-1)*100 + "%");
+
+    progress.style.width = ((actives.length-1)/ (circles.length-1)*100 + "%");
+
+    if(currentActive === 1){
+        prev.disabled = true;
+    }
+    else if(currentActive === circles.length){
+        next.disabled = true;
+    }
+    else{
+        prev.disabled = false;
+        next.disabled = false;
+    }
 }
 
 
